cc0cc78a29280f003fb70c0c5c7eab9f7e0b1fe6
Kuwarbir Singh <kuwarsehgal@gmail.com>
2019-03-11 14:02:49 +0530

    first java file added first commit

---
+++ HelloGit.java
@@ -0,0 +1,10 @@
+package first;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First java project upload");
+	}
+
+}
