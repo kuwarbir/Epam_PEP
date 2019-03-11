745eddab5471447ff7b00b01506590f6e3198787
Kuwarbir Singh <kuwarsehgal@gmail.com>
2019-03-11 14:20:55 +0530

    added another commit

--- HelloGit.java
+++ HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("First java project upload");
+int a=45;
+int b=50;
+System.out.print(a+b);
 	}
 
 }
